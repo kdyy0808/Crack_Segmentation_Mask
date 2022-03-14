faff4a0be9ac0e6bf4bb753034146c007a42633d
kdyy0808 <40755420+kdyy0808@users.noreply.github.com>
2022-03-14 10:01:58 +0900

    Create create_mask_prediction_diff.py

---
+++ create_mask_prediction_diff.py
@@ -0,0 +1,174 @@
+import os
+import cv2
+from cv2 import IMREAD_COLOR
+
+
+#os.environ['DISPLAY'] = ':0'
+#os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
+
+
+ori_folder = "/home/pirl/DH/pirl/CRACK_SEG/Crack_img_data/1st_training/test/ori_img/"
+gt_folder = "/home/pirl/DH/pirl/CRACK_SEG/Crack_img_data/1st_training/test/mask_img/"
+#predict_folder = "/home/pirl/DH/pirl/CRACK_SEG/Crack_img_data/2nd_training/train/prediction/"
+
+compare_result_folder = "/home/pirl/DH/pirl/CRACK_SEG/Crack_img_data/1st_training/test/compare_result2/"
+#
+#
+#
+#ori_folder = "/home/pirl/DH/pirl/CRACK_SEG/Crack_img_data/test/ori_img/"
+#gt_folder = "/home/pirl/DH/pirl/CRACK_SEG/Crack_img_data/test/mask_img/"
+#predict_folder = "/home/pirl/DH/pirl/CRACK_SEG/Crack_img_data/test/prediction/"
+#compare_result_folder = "/home/pirl/DH/pirl/CRACK_SEG/Crack_img_data/test/compare_result/"
+
+
+file_list = os.listdir(ori_folder)
+gt_file_list = os.listdir(gt_folder)
+#predict_file_list = os.listdir(predict_folder)
+
+
+file_list.sort()
+count=0
+file_num = len(file_list)
+
+for file_name in file_list:
+    if file_name in gt_file_list:
+        if file_name in file_list:# 이거 predict
+            img=cv2.imread(ori_folder+file_name,IMREAD_COLOR)
+            gt_img = cv2.imread(gt_folder+file_name,IMREAD_COLOR)## 1 0 
+            #predict_img = cv2.imread(predict_folder+file_name,IMREAD_COLOR)###132 197
+
+            b,g,r=cv2.split(gt_img)
+            #b_p,_,_ = cv2.split(predict_img)
+
+            #test = b+b_p
+
+
+            # TP = len(img[test==133])
+            # FP = len(img[test==132])
+            # FN = len(img[test==198])
+            # TN = len(img[test==197])
+
+            # if TP==0:
+            #     Precision = 0
+            #     Recall =0
+            #     Accuracy =0
+            #     IOU = 0
+
+            # else:
+
+            #     Precision = float(TP/(TP+FP))
+            #     Recall = float(TP/(TP+FN))
+            #     Accuracy = float((TP+TN)/(TP+FP+FN+TN))
+            #     IOU = float(TP/(TP+FN+FP))
+
+
+            blue = (255, 0, 0)
+            green= (0, 255, 0)
+            red= (0, 0, 255)
+            white= (255, 255, 255) 
+            # 폰트 지정
+            font =  cv2.FONT_HERSHEY_COMPLEX_SMALL
+            
+            # 이미지에 글자 합성하기
+            img[:100,:220] = (255,255,255)
+            # img = cv2.putText(img, "Precision : {:.3f}".format(Precision), (5, 25), font, 1, blue, 1)
+            # img = cv2.putText(img, "Recall    : {:.3f}".format(Recall), (5, 55), font, 1, blue, 1)
+            # img = cv2.putText(img, "IOU       : {:.3f}".format(IOU), (5, 85), font, 1, blue, 1)
+
+
+            #print(len(img[test==133]))
+            # img[test==133] = (0,255,0)#맞춘거 초록  
+            # img[test==198] = (0,0,255)#못맞춘거 (정답인데 체크못함) 붉은색
+            # img[test==132] = (255,0,0)# 정답으로 오해한거 하늘색 
+            
+            if "DY" not in file_name:
+                if "soo" not in file_name:
+                    img[b==1]=(255,0,0)
+                    cv2.imwrite(compare_result_folder+file_name,img)
+
+
+
+
+
+            #cv2.imshow("dd", img)
+
+            
+            #cv2.waitKey()
+            #cv2.destroyAllWindows()
+            count+=1
+            print("-----train_file working-----"+str(count)+"/"+str(file_num)+"----",end="")
+            print("\r", end="")
+print("")
+print("Complete!!")
+
+
+
+
+
+'''
+
+file_list = os.listdir(ori_folder)
+gt_file_list = os.listdir(gt_folder)
+predict_file_list = os.listdir(predict_folder)
+
+count=0
+total_num = len(file_list)
+
+file_list.sort()
+
+for file_name in file_list:
+    if file_name in gt_file_list:
+        if file_name in predict_file_list:
+            img=cv2.imread(ori_folder+file_name,IMREAD_COLOR)
+            gt_img = cv2.imread(gt_folder+file_name,IMREAD_COLOR)## 1 0 
+            predict_img = cv2.imread(predict_folder+file_name,IMREAD_COLOR)###132 197
+
+            b,g,r=cv2.split(gt_img)
+            b_p,_,_ = cv2.split(predict_img)
+
+            test = b+b_p
+
+            TP = len(img[test==133])
+            FP = len(img[test==132])
+            FN = len(img[test==198])
+            TN = len(img[test==197])
+
+            Precision = float(TP/(TP+FP))
+            Recall = float(TP/(TP+FN))
+            Accuracy = float((TP+TN)/(TP+FP+FN+TN))
+            IOU = float(TP/(TP+FN+FP))
+
+
+            blue = (255, 0, 0)
+            green= (0, 255, 0)
+            red= (0, 0, 255)
+            white= (255, 255, 255) 
+            # 폰트 지정
+            font =  cv2.FONT_HERSHEY_COMPLEX_SMALL
+            
+            # 이미지에 글자 합성하기
+            img[:100,:220] = (255,255,255)
+            img = cv2.putText(img, "Precision : {:.3f}".format(Precision), (5, 25), font, 1, blue, 1)
+            img = cv2.putText(img, "Recall    : {:.3f}".format(Recall), (5, 55), font, 1, blue, 1)
+            img = cv2.putText(img, "IOU       : {:.3f}".format(IOU), (5, 85), font, 1, blue, 1)
+
+
+            #print(len(img[test==133]))
+
+            img[test==133] = (0,255,0)#맞춘거 초록  
+            img[test==198] = (0,0,255)#못맞춘거 (정답인데 체크못함) 붉은색
+            img[test==132] = (255,0,0)# 정답으로 오해한거 하늘색 
+
+            count+=1
+
+            print("----"+str(count)+"/"+str(total_num)+"----",end="")
+            print("\r", end="")
+print("")
+print("Complete!!")
+            #cv2.imshow("dd", img)
+
+            #cv2.imwrite(compare_result_folder+file_name,img)
+            #cv2.waitKey()
+            #cv2.destroyAllWindows()
+
+'''
